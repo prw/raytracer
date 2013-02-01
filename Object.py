6924c258e722557bac996b87013fa4cbd015f658
Phil <timberline5@gmail.com>
2013-01-31 19:21:09 -0700

    working much much better

--- Object.py
+++ Object.py
@@ -1,19 +1,22 @@
 from Point import Point
 from Vector import Vector
 from Tuple import Tuple
+from Ray import Ray
+import math
 
 class Object(object):
-	def __init__(self, Kdiffuse=1.0, Kspecular=0.0, shinyness=1.0):
+	def __init__(self, color, Kdiffuse=1.0, Kspecular=0.0, shinyness=1.0):
 		self.diffuse=Kdiffuse
 		self.specular=Kspecular
 		self.shinyness=shinyness
+		self.color = color
 	
 	def intersect(self, ray):
 		pass
 
 class Sphere(Object):
-	def __init__(self, origin, r, diffuse=1.0, specular=0.0, shinyness=1.0):
-		super(Sphere, self).__init__(diffuse, specular, shinyness)
+	def __init__(self, origin, r, color, diffuse=1.0, specular=0.0, shinyness=1.0):
+		super(Sphere, self).__init__(color, diffuse, specular, shinyness)
 		self.origin = origin
 		self.r = r
 	
@@ -27,35 +30,33 @@ class Sphere(Object):
 		
 		c = dist[0]**2+dist[1]**2+dist[2]**2 - self.r**2
 		
-		disc = b*b-4*c
-		if disc < 0:
+		disc = b*b-4.0*c
+		if disc <= 0:
 			return -1
 		
 		disc = math.sqrt(disc)
 		
-		t0 = (-b-disc/2.0)
-		t1 = (-b+disc/2.0)
-		
-		if t0<t1 and t0>0:
+		t0 = (-b-disc)/2.0
+		if t0 > 0:
 			return t0
-		else:
-			return t1
-	
-	def getReflected(self, ray, t):
+		t1 = (-b+disc)/2.0
+		
+		return t1
+		
+	def getReflectedNormal(self, ray, t):
 		# need to find the point, then find normal at point
 		#then get reflected ray, origin=pt
-		p = Point(*rad.d.scale(t).v)
-		
-		normal = Vector((p-self.origin).v).normalize()
+		p = Point(*(ray.d.scale(t)).v )+ray.o
+		normal = Vector(*(p-self.origin).v)
+
+		normal.normalize()
 		
 		rr = Ray()
 		
 		rr.o=p
 		
-		normal.iscale(2*r.d.dot(normal))
-		
-		rr.d = rr.d - normal
-		
-		return rr
+		rr.d = ray.d - normal.scale(2*ray.d.dot(normal))
+		rr.d=Vector(*rr.d.v)
+		return rr, normal
 		
 		
--- Tracer.py
+++ Tracer.py
@@ -2,33 +2,35 @@
 from Ray import *
 from Object import *
 from Point import *
+from Light import *
+from random import random
 
 class Window:
 	def __init__(self, x, y):
 		self.xmax=float(x)
 		self.ymax=float(y)
-		self.viewpoint = Point(1,0,0)
+		self.viewpoint = Point(0,0,.9)
 		
 		self.window = [[Tuple(0,0,0) for i in range(y)] for i in range(x)]
 	
 	def getRay(self, x,y):
-		p = Point(.5+(x/self.xmax), (y/self.ymax)-0.5, 0.0)
+		p = Point((x/self.xmax)-0.5, (y/self.ymax)-0.5, 0.0)
 		
 		r = Ray()
 		
 		r.o=self.viewpoint
-		r.d=Vector((self.viewpoint - p).v)
+		r.d=Vector(*(p-self.viewpoint).v)
 		
 		return r
 	
 	def outputPPM(self):
-		print("P3\n{} {}\n255".format(self.xmax, self.ymax))
+		print("P3\n{} {}\n255".format(int(self.xmax), int(self.ymax)))
 		
 		for i in range(int(self.xmax)):
 			for j in range(int(self.ymax)):
 				color = self.window[i][j].v
-				color = [int(color[i]*255) for i in range(3)]
-				print color, 
+				color = [min(int(color[k]*255.0), 255) for k in range(3)]
+				print "{} {} {} ".format(color[0],color[1], color[2]),  
 			
 			print("")
 	
@@ -37,29 +39,87 @@ class Window:
 class Tracer:
 	def __init__(self, x, y):
 		self.objects = []
+		self.lights = []
 		self.w = Window(x,y)
+		self.recursionLimit=10
 	
 	def addObject(self, o):
-		self.objects.append(o)
+		self.objects.append(o)	
+	def addLight(self, l):
+		self.lights.append(l)
+	
+	def _trace(self, r, intensityList, recursion):
+		r.d.normalize()
+		close = float("inf")
+		closeObj = None
+		
+		for obj in self.objects: #find the closest intersection
+			t = obj.intersect(r)
+			if t<close and t>0.0:
+				close = t
+				closeObj = obj
+		
+		if closeObj != None: # if there was in fact an intersection do this...
+			close-=.000001
+			rr,normal = closeObj.getReflectedNormal(r, close) #calculate reflected + normal vector
+			intersectPoint = rr.o
+			
+			intensityList.append(IntensityRecord(closeObj.color))
+			
+			for l in self.lights:
+				inshade = False
+				Lray = Ray(intersectPoint, Vector(*(l.pos-intersectPoint).v))
+				Ldist = Lray.d.length()
+				Lray.d.normalize()
+				#print "lray: ", Lray.d.v, Lray.o.v
+				for obj in self.objects:
+					odist = obj.intersect(Lray)
+					if odist>0.001:
+						#print odist
+						inshade = True
+						
+				
+				if not inshade:
+					#Lray.d.negate()
+					Lscale = normal.dot(Lray.d)
+					#print "normal dot lray", normal.dot(Lray.d)
+					#print "normal: ", normal.v, "Lray.d", Lray.d.v
+					if Lscale >0.01:
+						intensityList[-1].intensity+= l.intensity.scale(Lscale)
+				else:
+					intensityList[-1].intensity+=Tuple(.1,.1,.1)
+			
+			#so after we do all the lights then we can use the reflected ray to trace again
+			#if recursion < self.recursionLimit:
+			#	self._trace(rr, intensityList, recursion+1)
+					
+					
 	
 	def trace(self):
 		for i in range(int(self.w.xmax)):
 			for j in range(int(self.w.ymax)):
-				close = float("inf")
-				closeO = None
+				#print i,j, 
 				r = self.w.getRay(i,j)
-				r.d.normalize()
-				for obj in self.objects:
-					t = obj.intersect(r)
-					if t<close and t>0:
-						t=close
-						closeO = obj
+				intensityList = []
+				self._trace(r, intensityList, 8)
+				intensity = Tuple(0.0,0.0,0.0)
+				while( len(intensityList) > 0 ):
+					curin = intensityList.pop()
+					#print curin.intensity.v
+					intensity+= curin.getIntensity()
 				
-				if closeO != None:
-					self.w.window[i][j][0]=1.0
+				self.w.window[i][j] = intensity
 				
-t = Tracer(100,100)
-t.addObject(Sphere( Point(0.0,0.0,-5.0), 2.0))
+				
+t = Tracer(700,700)
+for i in range(10):
+	t.addObject(Sphere( Point(2.5-random()*5.,2.5-random()*5,-13.0+random()*5), random()*0.3+1., 
+		Tuple(random()*.5+.4,random()*.5+.4,random()*.5+.4)))
+#t.addObject(Sphere( Point(1.0,0.0,-10.0), 2.0, Tuple(1,0,0)))
+#t.addObject(Sphere( Point(-3.0,0.0,-10.0), 2.0, Tuple(1,0,1)))
+#t.addObject(Sphere( Point(3.5,-0.0,-8.0), .25, Tuple(.2,.5,1)))
+#t.addLight(Light(Point(-10,-10,-15.5), Tuple(.4,.4,.4)))
+t.addLight(Light(Point(10.,10.,45.), Tuple(.8,.8,.8)))
 
 t.trace()
 
